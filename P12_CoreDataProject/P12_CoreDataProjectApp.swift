39b4dff7280fde0e7e9097ab051e17c1df62b958
Jae Cho <jjaecho@gmail.com>
2022-06-02 21:20:26 -0400

    Initial Commit

---
+++ P12_CoreDataProject/P12_CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  P12_CoreDataProjectApp.swift
+//  P12_CoreDataProject
+//
+//  Created by Jae Cho on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct P12_CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
